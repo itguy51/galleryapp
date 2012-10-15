33826db33280e24102754e0517629b1a4a582e5a
Josh Pruim <josh@josh-M11x-R2.(none)>
2012-10-15 13:53:41 -0700

    Initial Commit

---
+++ app/models/image.rb
@@ -0,0 +1,3 @@
+class Image < ActiveRecord::Base
+  attr_accessible :desc, :file, :title
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galleryapp::Application.config.secret_token = '56a7a9b2a3c67c6408bcd5f54e00df5a1f2cbf5645fc5804dbbcd0dce3e8faf34de37afcd782ae9d7c790acd4f650c3f6042c1008d412503c51197068de3f498'
