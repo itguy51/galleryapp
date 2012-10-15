6b4f247b40d508d4a3edb66d9a759e135478f796
Josh Pruim <josh@josh-M11x-R2.(none)>
2012-10-15 16:51:24 -0700

    Added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Galleryapp::Application.routes.draw do
+  devise_for :users
+
   resources :images
 
   resources :pages
@@ -52,7 +54,7 @@ Galleryapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
